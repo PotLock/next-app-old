abb159d5615158ec095352eca58edbc011dd00f7
phuongfunny <vovanphuong101098@gmail.com>
2024-01-09 22:18:53 +0700

    fix: reconfig prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,12 @@ import Banner from "@/components/Banner";
 import Button from "@/components/Button";
 import FeaturedProject from "@/components/FeaturedProject";
 
-
 export default function Home() {
   return (
     <main className="bg-[url('/background.png')] bg-cover bg-no-repeat flex w-full h-full flex-col  sm:px-[77px]">
-      <Banner/>
-      <FeaturedProject/>
-      <AllProject/>
+      <Banner />
+      <FeaturedProject />
+      <AllProject />
     </main>
-  )
+  );
 }

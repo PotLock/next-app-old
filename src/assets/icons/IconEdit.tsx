264da7478a500f2f6a4e48f2175492be36d0cb0c
hdntan <hdntan0903@gmail.com>
2024-01-10 18:36:04 +0700

    feat: add icon & img

---
+++ src/assets/icons/IconEdit.tsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+
+export interface IIconEditProps {}
+
+export default function IconEdit(props: IIconEditProps) {
+  return (
+    <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M2.24902 15.7509H5.06152L13.3565 7.4559L10.544 4.6434L2.24902 12.9384V15.7509ZM3.74902 13.5609L10.544 6.7659L11.234 7.4559L4.43902 14.2509H3.74902V13.5609Z" fill="#7B7B7B"/>
+    <path d="M13.7765 2.4684C13.484 2.1759 13.0115 2.1759 12.719 2.4684L11.3465 3.8409L14.159 6.6534L15.5315 5.2809C15.824 4.9884 15.824 4.5159 15.5315 4.2234L13.7765 2.4684Z" fill="#7B7B7B"/>
+    </svg>
+    
+
+
+    
+  );
+}

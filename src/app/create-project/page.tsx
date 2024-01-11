5f76ad9a63a95a3cd88a6c60ebbe3e25b9075edc
hdntan <hdntan0903@gmail.com>
2024-01-11 22:22:48 +0700

    feat: add  file create project

---
+++ src/app/create-project/page.tsx
@@ -0,0 +1,12 @@
+import BannerCreateProject from '@/views/CreateProject/Banner'
+import React from 'react'
+
+const CreateProject = () => {
+  return (
+    <div className='w-full h-full'>
+      <BannerCreateProject/>
+    </div>
+  )
+}
+
+export default CreateProject

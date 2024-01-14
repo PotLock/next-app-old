b23d5594371f309289a23f4e0ebd955c13d345ab
phuongfunny <vovanphuong101098@gmail.com>
2024-01-14 16:29:52 +0700

    feat: build modal login wallet

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -8,7 +8,6 @@ import {
   NavbarBrand,
   NavbarContent,
   NavbarItem,
-
   Button,
   NavbarMenuToggle,
   NavbarMenu,
@@ -16,13 +15,64 @@ import {
   useDisclosure,
   Badge,
 } from "@nextui-org/react";
-import React from "react";
+import React, { useEffect } from "react";
 import ModelCart from "./components/ModelCart";
 import Link from "next/link";
+import { setupWalletSelector } from "@near-wallet-selector/core";
+import { setupModal } from "@near-wallet-selector/modal-ui";
+import { setupNearWallet } from "@near-wallet-selector/near-wallet";
+import { setupMyNearWallet } from "@near-wallet-selector/my-near-wallet";
+import { setupSender } from "@near-wallet-selector/sender";
+import { setupHereWallet } from "@near-wallet-selector/here-wallet";
+import { setupMathWallet } from "@near-wallet-selector/math-wallet";
+import { setupNightly } from "@near-wallet-selector/nightly";
+import { setupMeteorWallet } from "@near-wallet-selector/meteor-wallet";
+
+import { setupLedger } from "@near-wallet-selector/ledger";
+import { setupWalletConnect } from "@near-wallet-selector/wallet-connect";
+import { setupNearFi } from "@near-wallet-selector/nearfi";
+import { setupCoin98Wallet } from "@near-wallet-selector/coin98-wallet";
 
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
+
+  const wallet = async () => {
+    const selector = await setupWalletSelector({
+      network: "testnet",
+      modules: [
+        setupNearWallet(),
+        setupMyNearWallet(),
+        setupSender(),
+        setupHereWallet(),
+        setupMathWallet(),
+        setupNightly(),
+        setupMeteorWallet(),
+        setupLedger(),
+        setupNearFi(),
+        setupCoin98Wallet(),
+        // setupWalletConnect({
+        //   projectId: "c4f79cc...",
+        //   metadata: {
+        //     name: "NEAR Wallet Selector",
+        //     description: "Example dApp used by NEAR Wallet Selector",
+        //     url: "https://github.com/near/wallet-selector",
+        //     icons: ["https://avatars.githubusercontent.com/u/37784886"],
+        //   },
+        // }),
+      ],
+    });
+
+    const modal = setupModal(selector, {
+      contractId: "test.testnet",
+    });
+    modal.show();
+  };
+
+  useEffect(() => {
+    wallet();
+  });
+
   return (
     <Navbar
       maxWidth="full"
@@ -33,14 +83,13 @@ const Header = () => {
       <ModelCart isOpen={isOpen} onOpenChange={onOpenChange} />
 
       <NavbarContent justify="start">
-        <NavbarBrand >
+        <NavbarBrand>
           <Link href="/" className="flex gap-2">
-          <div className="mb-1">
-            <IconLogo />
-          </div>
-          <p className="font-bold text-inherit">POTLOCK</p>
+            <div className="mb-1">
+              <IconLogo />
+            </div>
+            <p className="font-bold text-inherit">POTLOCK</p>
           </Link>
-         
         </NavbarBrand>
       </NavbarContent>
 
@@ -68,7 +117,7 @@ const Header = () => {
       <NavbarMenu className="w-full h-full bg-[white] flex items-center justify-center gap-10">
         {MENUITEMS.map((item, index) => (
           <NavbarMenuItem key={index}>
-            <Link className="w-full font-semibold text-4xl" href="#" >
+            <Link className="w-full font-semibold text-4xl" href="#">
               {item}
             </Link>
           </NavbarMenuItem>

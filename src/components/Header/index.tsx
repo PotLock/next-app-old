efbcd4bf7233cf70369a2ad7ab2a5058c7bc7db6
phuongfunny <vovanphuong101098@gmail.com>
2024-01-15 19:52:38 +0700

    feat: config near wallet

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,77 +1,27 @@
 "use client";
-
 import IconLogo from "@/assets/icons/IconLogo";
 import IconMenu from "@/assets/icons/IconMenu";
+import wallet from "@/configs/near.config";
 import { MENUITEMS } from "@/constant";
 import {
+  Badge,
+  Button,
   Navbar,
   NavbarBrand,
   NavbarContent,
   NavbarItem,
-  Button,
-  NavbarMenuToggle,
   NavbarMenu,
   NavbarMenuItem,
+  NavbarMenuToggle,
   useDisclosure,
-  Badge,
 } from "@nextui-org/react";
-import React, { useEffect } from "react";
-import ModelCart from "./components/ModelCart";
 import Link from "next/link";
-import { setupWalletSelector } from "@near-wallet-selector/core";
-import { setupModal } from "@near-wallet-selector/modal-ui";
-import { setupNearWallet } from "@near-wallet-selector/near-wallet";
-import { setupMyNearWallet } from "@near-wallet-selector/my-near-wallet";
-import { setupSender } from "@near-wallet-selector/sender";
-import { setupHereWallet } from "@near-wallet-selector/here-wallet";
-import { setupMathWallet } from "@near-wallet-selector/math-wallet";
-import { setupNightly } from "@near-wallet-selector/nightly";
-import { setupMeteorWallet } from "@near-wallet-selector/meteor-wallet";
-
-import { setupLedger } from "@near-wallet-selector/ledger";
-import { setupWalletConnect } from "@near-wallet-selector/wallet-connect";
-import { setupNearFi } from "@near-wallet-selector/nearfi";
-import { setupCoin98Wallet } from "@near-wallet-selector/coin98-wallet";
+import React from "react";
+import ModelCart from "./components/ModelCart";
 
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
-
-  const wallet = async () => {
-    const selector = await setupWalletSelector({
-      network: "testnet",
-      modules: [
-        setupNearWallet(),
-        setupMyNearWallet(),
-        setupSender(),
-        setupHereWallet(),
-        setupMathWallet(),
-        setupNightly(),
-        setupMeteorWallet(),
-        setupLedger(),
-        setupNearFi(),
-        setupCoin98Wallet(),
-        // setupWalletConnect({
-        //   projectId: "c4f79cc...",
-        //   metadata: {
-        //     name: "NEAR Wallet Selector",
-        //     description: "Example dApp used by NEAR Wallet Selector",
-        //     url: "https://github.com/near/wallet-selector",
-        //     icons: ["https://avatars.githubusercontent.com/u/37784886"],
-        //   },
-        // }),
-      ],
-    });
-
-    const modal = setupModal(selector, {
-      contractId: "test.testnet",
-    });
-    modal.show();
-  };
-
-  useEffect(() => {
-    wallet();
-  });
 
   return (
     <Navbar
@@ -106,6 +56,9 @@ const Header = () => {
           <Badge content="5" color="warning" variant="solid">
             <Button onClick={onOpen}>CART</Button>
           </Badge>
+          <Button className="ml-5" color="primary" onClick={wallet}>
+            Login
+          </Button>
         </NavbarItem>
       </NavbarContent>
 
